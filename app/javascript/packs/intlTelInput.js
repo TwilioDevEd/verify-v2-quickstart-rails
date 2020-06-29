0e5a052f13a118a7d6f0984ec15df828ad5513c0
Angela Rojas <arojas@stackbuilders.com>
2020-06-29 15:09:57 -0500

    Move js assets to javascript folder

--- app/javascript/packs/intlTelInput.js
+++ app/javascript/packs/intlTelInput.js
@@ -7,7 +7,8 @@
 // wrap in UMD
 (function(factory) {
     var intlTelInput = factory(window, document);
-    if (typeof module === "object" && module.exports) module.exports = intlTelInput; else window.intlTelInput = intlTelInput;
+    // if (typeof module === "object" && module.exports) module.exports = intlTelInput; else window.intlTelInput = intlTelInput;
+    window.intlTelInput = intlTelInput;
 })(function(window, document, undefined) {
     "use strict";
     return function() {
