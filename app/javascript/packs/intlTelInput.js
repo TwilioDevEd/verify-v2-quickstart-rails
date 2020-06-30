4a8b481403dd1e071b274b84033e14928b3328e5
Angela Rojas <arojas@stackbuilders.com>
2020-06-30 09:12:25 -0500

    Remove commented line

--- app/javascript/packs/intlTelInput.js
+++ app/javascript/packs/intlTelInput.js
@@ -7,7 +7,6 @@
 // wrap in UMD
 (function(factory) {
     var intlTelInput = factory(window, document);
-    // if (typeof module === "object" && module.exports) module.exports = intlTelInput; else window.intlTelInput = intlTelInput;
     window.intlTelInput = intlTelInput;
 })(function(window, document, undefined) {
     "use strict";
